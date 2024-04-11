2cd31158e545a4d24db6f0d88c568dfd50d31347
Atomic Junky <67459553+atomic-junky@users.noreply.github.com>
2024-04-11 13:40:24 +0200

    Update build and add an icon

--- tasty_supplies/main.py
+++ tasty_supplies/main.py
@@ -1,2 +1,5 @@
-from beet import ResourcePack
+from beet import ResourcePack, PngFile, Context
 
+
+def build_resource_pack(ctx: Context):
+    ctx.assets.icon = PngFile(source_path="tasty_supplies/pack.png")
